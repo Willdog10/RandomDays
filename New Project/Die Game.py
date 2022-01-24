4d90831aadead3faf009afb45344ccfddf9b2e57
William <willdog18@gmail.com>
2022-01-24 00:51:09 -0500

    yes

---
+++ New Project/Die Game.py
@@ -0,0 +1,14 @@
+import random
+
+die1 = random.randint (1,6)
+die2 = random.randint (1,6)
+die3 = random.randint (1,6)
+
+print (die1, die2, die3)
+print (die1 + die2 + die3)
+
+if (die1 + die2 + die3) == 7 or (die1 + die2 + die3)== 11:
+    
+    print ("you Win")
+
+    print ("you Lost")
