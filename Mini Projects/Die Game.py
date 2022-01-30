5278bca3e1a69a6fa6f021678258d3227884cf1a
William <willdog18@gmail.com>
2022-01-29 23:56:21 -0500

    enter

--- Mini Projects/Die Game.py
+++ Mini Projects/Die Game.py
@@ -1,4 +1,5 @@
 import random
+import secrets
 
 die1 = random.randint (1,6)
 die2 = random.randint (1,6)
@@ -7,8 +8,7 @@ die3 = random.randint (1,6)
 print (die1, die2, die3)
 print (die1 + die2 + die3)
 
-if (die1 + die2 + die3) == 7 or (die1 + die2 + die3)== 11:
-    
-    print ("you Win")
+if (die1 + die2 + die3) != 7 or (die1 + die2 + die3)== 11:
 
-    print ("you Lost")
+(random.choice)= ('W','L')
+(print)"random.choice"('W,L'))
