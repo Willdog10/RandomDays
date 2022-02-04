40a509005b3df11975aaaba1276f659a16d2e3e4
William <willdog18@gmail.com>
2022-02-03 22:28:29 -0500

    enter

---
+++ Mini Projects/RandomDaysOfTheWeekGenerator.py
@@ -0,0 +1,22 @@
+from calendar import FRIDAY, MONDAY, THURSDAY, TUESDAY, WEDNESDAY
+from random import random
+
+today1 = 'MONDAY'
+print (today1)
+
+today2 = 'TUESDAY'
+print (today2)
+
+today3 = 'WEDNESDAY'
+print (today3)
+
+today4 = 'THURSDAY'
+print (today4)
+
+today5 = 'FRIDAY'
+print (today5)
+
+#Guess the random day of the week 
+
+WORDS = ("MONDAY", "TUESDAY","WEDNESDAY","THURSDAY","FRIDAY")
+
